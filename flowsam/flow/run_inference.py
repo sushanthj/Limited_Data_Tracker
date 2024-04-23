e10da09b6352d8efb6799b4c0ee7fdb621aa2d56
sushanth <sushanth.jayanth@gmail.com>
2024-04-23 13:12:37 -0400

    added flowsam

---
+++ flowsam/flow/run_inference.py
@@ -0,0 +1,33 @@
+import os
+import glob as gb
+import sys
+
+sys.path.append("..")
+
+data_path = './flowsam_data'
+rgb_path = data_path + '/JPEGImages'
+# '/JPEGImages/480p' for DAVIS-related datasets and '/JPEGImages' for others
+
+gap = [1]
+reverse = [0, 1]
+batch_size = 4
+
+folder = gb.glob(os.path.join(rgb_path, '*'))
+for r in reverse:
+    for g in gap:
+        for f in folder:
+            print('===> Runing {}, gap {}'.format(f, g))
+            model = './raft-kitti.pth'  # model
+            if r==1:
+                raw_outroot = data_path + '/Flows_gap-{}/'.format(g)  # where to raw flow
+                outroot = data_path + '/FlowImages_gap-{}/'.format(g)  # where to save the image flow
+            elif r==0:
+                raw_outroot = data_path + '/Flows_gap{}/'.format(g)   # where to raw flow
+                outroot = data_path + '/FlowImages_gap{}/'.format(g)   # where to save the image flow
+
+            os.system("python3 predict.py "
+                        "--gap {} --model {} --path {} --batch_size {} "
+                        "--outroot {} --reverse {} --raw_outroot {}".format(g, model, f, batch_size, outroot, r, raw_outroot))
+
+
+
